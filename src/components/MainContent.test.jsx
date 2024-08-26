f38ad241a3326a309af73f4c5af3c239bca9a371
devsecops00 <devsecops00@hotmail.com>
2024-08-26 22:18:54 +0800

    updated comment

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,7 +4,7 @@ import userEvent from "@testing-library/user-event";
 
 import MainContent from "./MainContent";
 
-// My test!
+// My test!!!
 
 describe("MainContent", () => {
   it("should render a button", () => {
